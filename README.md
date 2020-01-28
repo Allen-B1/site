4949baeaff1e504f07c7cb17a550b1b11f53cd0d
Allen B <pingguo.ny@gmail.com>
2020-01-27 21:30:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jellyfish Site
+This is the site for FTC Team 4654, Jellyfish. If you'd like to repurpose this site for your own use, you may do so.
+
+## Rules
+The templating rules follow go's [`html/template`](https://godoc.org/html/template). Navigating to a path on the URL will serve the file on that path that is present on the filesystem. The exception is `.html` files - the URL does not contain the extension.
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+	"html/template"
+)
+
+func listFiles(dir string) ([]string, error) {
+	out := make([]string, 0)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't list files: %w", err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			subFiles, err := listFiles(filepath.Join(dir, file.Name()))
+			if err != nil {
+				return nil, err
+			}
+			for _, subFile := range subFiles {
+				out = append(out, filepath.Join(file.Name(), subFile))
+			}
+		} else if !strings.HasSuffix(file.Name(), "~") {
+			out = append(out, file.Name())
+		}
+	}
+	return out, nil
+}
+
+// Type FileHandler serves a file.
+type FileHandler string
+
+func (f FileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadFile(string(f))
+	if err != nil {
+		w.WriteHeader(500)
+		io.WriteString(w, "internal server error")
+		log.Printf("error loading '%s': %v\n", string(f), err)
+		return
+	}
+	w.Header().Set("Content-Type", mime.TypeByExtension(filepath.Ext(string(f))))
+	w.WriteHeader(200)
+	w.Write(body)
+}
+
+var Templates = template.New("").Funcs(template.FuncMap{
+	"list": func (arr ...interface{}) []interface{} {
+		return arr
+	},
+})
+
+type TemplateHandler string
+func (t TemplateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := Templates.ExecuteTemplate(w, filepath.Base(string(t)), nil)
+	if err != nil {
+		log.Printf("error templating '%s': %v\n", string(t), err)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		ReadHeaderTimeout: time.Second * 8,
+		WriteTimeout:      time.Second * 8,
+		Handler: mux,
+		Addr: ":8080",
+	}
+
+	files, err := listFiles(".")
+	if err != nil {
+		log.Println("fatal: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(files)
+	for _, file := range files {
+		if strings.HasSuffix(file, ".html") {
+			_, err = Templates.ParseFiles(file)
+			if err != nil {
+				log.Println("fatal: ", err)
+				os.Exit(1)
+			}
+			index := strings.Index(file, ".")
+			name := file
+			if index >= 0 {
+				name = file[:index]
+			}
+			mux.Handle("/" + name, TemplateHandler(file))
+		} else {
+			mux.Handle("/" + file, FileHandler(file))
+		}
+	}
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			TemplateHandler("index.html").ServeHTTP(w, r)			
+		} else {
+			w.Header().Set("Location", "/")
+			w.WriteHeader(303)
+		}
+	})
+	server.ListenAndServe()
+}
